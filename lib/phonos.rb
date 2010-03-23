fcde6e21e8ef6571f4fe3529f26df4d801da7428
Dmitry A. Ustalov <eveeel@gmail.com>
2010-03-23 21:16:26 +0500

    careful phonos initializer

--- lib/phonos.rb
+++ lib/phonos.rb
@@ -1,5 +1,12 @@
 # encoding: utf-8
 
+begin
+  require 'active_support/core_ext'
+rescue LoadError
+  require 'rubygems'
+  retry
+end
+
 module Phonos
   require 'phonos/main'
   require 'phonos/language'
