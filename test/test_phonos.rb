589d63f6e2c4faac2fd5f8e26fbc472fd1c04192
Dmitry A. Ustalov <eveeel@gmail.com>
2010-03-23 21:15:52 +0500

    analyzer singleton evaluation

--- test/test_phonos.rb
+++ test/test_phonos.rb
@@ -1,29 +1,28 @@
-#encoding: utf-8
-$KCODE = 'u'
+# encoding: utf-8
 
 require 'test/helper'
 
 class TestPhonos < Test::Unit::TestCase
   context "Phonos" do
-    setup do
-      @phonos = TestAnalyzer.instance
-    end
-    
+    subject { Phonos::Analyzer.instance }
+
     context "String filtrator" do
       should "return prepared string" do
-        assert_equal "буба сука ДеБил".mb_chars, @phonos.prepare("  БуБа   сука@    ^дебиЛ ".mb_chars)
+        assert_equal "буба сука ДеБил".mb_chars,
+          subject.prepare("  БуБа   сука@    ^дебиЛ ".mb_chars)
       end
     end
 
     context "Counter" do
       should "return correct hash" do
-        assert_equal({ 'б' => { :abs => 2, :rel => 4 }, 'у' => { :abs => 2, :rel => 2 },
-            'а' => { :abs => 2, :rel => 2}, 'с' => { :abs => 1, :rel => 3 },
-            'к' => { :abs => 1, :rel => 1 }, 'Д' => { :abs => 1, :rel => 3 },
-            'е' => { :abs => 1, :rel => 1 }, 'Б' => { :abs => 1, :rel => 1 },
-            'и' => { :abs => 1, :rel => 1 }, 'л' => { :abs => 1, :rel => 1 },
-            :space => 2, :total => 13
-          }, @phonos.count("буба сука ДеБил".mb_chars))
+        assert_equal({
+          'б' => { :abs => 2, :rel => 4 }, 'у' => { :abs => 2, :rel => 2 },
+          'а' => { :abs => 2, :rel => 2}, 'с' => { :abs => 1, :rel => 3 },
+          'к' => { :abs => 1, :rel => 1 }, 'Д' => { :abs => 1, :rel => 3 },
+          'е' => { :abs => 1, :rel => 1 }, 'Б' => { :abs => 1, :rel => 1 },
+          'и' => { :abs => 1, :rel => 1 }, 'л' => { :abs => 1, :rel => 1 },
+          :space => 2, :total => 13 },
+          subject.count("буба сука ДеБил".mb_chars))
       end
     end
   end
