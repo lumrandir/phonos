f4e0999d78446df1fac5be75b574f7c2f7737173
Dmitry A. Ustalov <eveeel@gmail.com>
2010-03-23 21:16:36 +0500

    analyzer code refactoring

--- lib/phonos/main.rb
+++ lib/phonos/main.rb
@@ -1,19 +1,14 @@
 # encoding: utf-8
 $KCODE = 'u'
 
-begin
-  require 'active_support/core_ext'
-  require 'mathn'
-rescue LoadError
-  require 'rubygems'
-  retry
-end
 =begin rdoc
-Simple phonosemantic analyzer. All rights reserved by Liebermann Inc. and
-Sinagoga Ltd.
+  Simple phonosemantic analyzer.
+  All rights reserved by Liebermann Inc. and Sinagoga Ltd.
+  Developed by Konstantin "Lumren Randir" Lukinskih.
 =end
 module Phonos
   require 'singleton'
+  require 'mathn'
 
   SCALES = [
     :good, :big, :gentle, :feminine, :light, :active, :simple, :strong, :hot,
@@ -34,7 +29,7 @@ require 'phonos'
   class Analyzer
     include Singleton
     def initialize
-      @base = Phonos::Language::RUSSIAN
+      @lang = Phonos::Language::RUSSIAN
     end
 
 =begin rdoc
@@ -52,69 +47,66 @@ Scale-keys:
   :loud, :long, :brave, :kind, :mighty, :mobile
 =end
     def analyse text
-      @text = prepare text.mb_chars
-      process(@text.delete(' '), count(@text))
+      counts = count(text.mb_chars)
+      f1 = {}
+      f2 = {}
+      # и запомните, мои маленькие дэткорщики,- главное - ЕБАШИЛОВО!
+      prepare(text.mb_chars).delete(' ').each_char do |c|
+        char = counts[c]
+        SCALES.each do |scale|
+          f1[scale] ||= 0
+          f2[scale] ||= 0
+          if counts[:space] <= 4 && char[:abs] / counts[:total] > 0.368
+            f1[scale] += (@lang[c][scale] * char[:rel] / char[:abs] * (-0.368)) /
+              (@lang[c][:frequency] * Math.log(@lang[c][:frequency]))
+            f2[scale] += (char[:rel] / char[:abs] * (-0.368)) /
+              (@lang[c][:frequency] * Math.log(@lang[c][:frequency]))
+          else
+            f1[scale] += (@lang[c][scale] * char[:rel] / counts[:total] *
+              Math.log(char[:abs] / counts[:total])) / (@lang[c][:frequency] *
+              Math.log(@lang[c][:frequency]))
+            f2[scale] += (char[:rel] / counts[:total] *
+              Math.log(char[:abs] / counts[:total])) / (@lang[c][:frequency] *
+              Math.log(@lang[c][:frequency]))
+          end
+        end
+      end
+      SCALES.inject({}) do |r, scale|
+        r[scale] = 3.0 - f1[scale] / f2[scale]
+        r
+      end
     end
 
     def prepare text
       # обрезаем, приводим в нижний регистр, отсекаем нахер лишние символы
       # и выделяем верхним регистром мягкие согласные
-      text.strip.downcase.gsub(/[^а-я\s]/, "").gsub(
-        /[бвгджзклмнпрстфхцчшщ][еёиьюя]/) { |match| match.mb_chars.capitalize }.gsub(/\s{2,}/, " ")
+      text.strip.downcase.
+        gsub(/[^а-я\s]/, "").
+        gsub(/[бвгджзклмнпрстфхцчшщ][еёиьюя]/) do |match|
+          match.mb_chars.capitalize
+        end.gsub(/\s{2,}/, " ")
     end
     private :prepare
-    
+
     def count text
-      @counts = {}
+      counts = {}
       # ищем число вхождений для каждого символа
       # из-за того, что метод String#count отказывается работать как надо,
       # придётся сделать через анус
       text.split(' ').each do |word|
         word.each_char do |c|
-          @counts[c] ||= {}
-          @counts[c][:abs] ||= 0
-          @counts[c][:rel] ||= 0
-          @counts[c][:rel] += 1
-          @counts[c][:abs] += 1
+          counts[c] ||= {}
+          counts[c][:abs] ||= 0
+          counts[c][:rel] ||= 0
+          counts[c][:rel] += 1
+          counts[c][:abs] += 1
         end
-        @counts[word[0].wrapped_string][:rel] += 2
+        counts[word[0].wrapped_string][:rel] += 2
       end
-      @counts[:space] = text.count ' '
-      @counts[:total] = text.size - @counts[:space]
-      @counts
+      counts[:space] = text.count ' '
+      counts[:total] = text.size - counts[:space]
+      counts
     end
     private :count
-
-    def process text, counts
-      @result = {}
-      @f1 = {}
-      @f2 = {}
-      # и запомните, мои маленькие дэткорщики,- главное - ЕБАШИЛОВО!
-      text.each_char do |c|
-        SCALES.each do |scale|
-          @f1[scale] ||= 0
-          @f2[scale] ||= 0
-          if counts[:space] <= 4 && counts[c][:abs] / counts[:total] > 0.368
-            @f1[scale] += (@base[c][scale] * counts[c][:rel] / counts[c][:abs] * (-0.368)) /
-              (@base[c][:frequency] * Math.log(@base[c][:frequency]))
-            @f2[scale] += (counts[c][:rel] / counts[c][:abs] * (-0.368)) /
-              (@base[c][:frequency] * Math.log(@base[c][:frequency]))
-          else
-            @f1[scale] += (@base[c][scale] * counts[c][:rel] / counts[:total] *
-              Math.log(counts[c][:abs] / counts[:total])) / (@base[c][:frequency] *
-              Math.log(@base[c][:frequency]))
-            @f2[scale] += (counts[c][:rel] / counts[:total] *
-              Math.log(counts[c][:abs] / counts[:total])) / (@base[c][:frequency] *
-              Math.log(@base[c][:frequency]))
-          end
-        end
-      end
-      SCALES.each do |scale|
-        @result[scale] = 3 - @f1[scale] / @f2[scale]
-      end
-      @result
-    end
-    private :process
-    
   end
 end
